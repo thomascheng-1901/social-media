5536961ec322945e10000f270172df6f53ad4417
Thomas <93049032+Thomas-HAHA@users.noreply.github.com>
2024-02-19 00:11:18 +0800

    comment section coloring

--- client/src/components/commentSection.jsx
+++ client/src/components/commentSection.jsx
@@ -8,10 +8,12 @@ const commentSection = (props) => {
         setShowComments(!showComments);
     }
 
+    let position = 0;
+
     return (
         <div>
             {!showComments && <button onClick={toggle}>Comments: ({props.comments.length})</button>}
-            {showComments && <button onClick={toggle}><div className='text-left'>{props.comments.map((c) => <p>{c}</p>)}</div></button>}
+            {showComments && <button onClick={toggle}><div className='text-left'>{props.comments.map((c) => {position += 1; return <p className={position % 2 === 0?'text-black':'text-gray-400'}>{c}</p>})}</div></button>}
         </div>
     )
 }
